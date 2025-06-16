10fa39b6b2d788f3fe3d583cd19412802f73193d
rrmqa12 <roger.mazzali@amorsaude.com>
2025-06-16 17:16:27 -0300

    Plano de Contas

---
+++ cypress/e2e/AWS/plano_de_contas.cy.js
@@ -0,0 +1,55 @@
+/// <reference types= "cypress" /> 
+
+describe('Módulo - Regime Tributário - Retorna uma lista ', () => {
+
+  beforeEach(() => {
+    cy.login()
+    cy.refreshToken()
+  });
+//
+  describe('Módulo - Recursos', () => {
+
+    it('Validar retorno 200', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/regimeTributario',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((response) => {
+        // Verifica se o status é 200
+        expect(response.status).to.eq(200);
+
+        // Verifica a estrutura dos dados
+        response.body.forEach((regime) => {
+          expect(regime).to.have.property('id');
+
+
+        });
+      });
+    });
+
+    it('Validar retorno 404', () => {
+      const token = Cypress.env('access_toke');
+
+      cy.request({
+        method: 'PUT',
+        url: '/api/v1/regimeTributario',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }, failOnStatusCode: false
+      }).then((response) => {
+        // Verifica se o status é 200
+        expect(response.status).to.eq(404);
+
+
+      });
+    });
+
+  });
+
+});
+
+
