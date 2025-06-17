afe7ddfd5c08b1921905ff15e0a920e34a301bbf
Ivan <ivan.santos@amorsaude.com>
2025-06-17 11:07:57 -0300

    Add Modulo Fuso Horario

---
+++ cypress/e2e/AWS/fuso_horario.cy.js
@@ -0,0 +1,77 @@
+/// <reference types= "cypress" /> 
+
+describe('Módulo - Fuso Horário - Retorna uma lista de regime tributários', () => {
+
+  beforeEach(() => {
+    cy.login()
+    cy.refreshToken()
+  });
+
+  describe('Módulo - Fuso Horário', () => {
+
+    it('Validar retorno 200', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/fusohorario',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((response) => {
+
+        // Validar status 200
+        expect(response.status).to.eq(200);
+
+        // Validar resposta exata esperada
+        const expectedResponse = [
+          {
+            "id": 1,
+            "fusohorario": "Horário do Acre (UTC-5)"
+          },
+          {
+            "id": 2,
+            "fusohorario": "Horário da Amazônia (UTC-4)"
+          },
+          {
+            "id": 3,
+            "fusohorario": "Horário de Brasília (UTC-3)"
+          },
+          {
+            "id": 4,
+            "fusohorario": "Horário de Fernando de Noronha (UTC-2)"
+          }
+        ];
+
+
+        // Validar cada item individualmente
+        response.body.forEach((item, index) => {
+          expect(item).to.have.property('id');
+          expect(item.id).to.be.a('number');
+
+          cy.log(`Item ${index + 1}:`, JSON.stringify(item));
+        });
+      });
+    });
+
+    it('Validar retorno 404', () => {
+
+      const token = Cypress.env('access_token')
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/fusohorario',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }, failOnStatusCode: false
+      }).then((response) => {
+        // Verificar se o status é 404
+        expect(response.status).to.eq(404);
+
+      });
+    })   
+
+  })
+})
+
+
+
--- cypress/e2e/AWS/plano_de_contas.cy.js
+++ cypress/e2e/AWS/plano_de_contas.cy.js
@@ -61,153 +61,155 @@ describe('Módulo - Plano de Contas', () => {
             });
         })
 
-        describe('Plano de Contas - Tipo Procedimento - Retorna os planos de contas para tipos de procedimentos', () => {
+    })
 
-            it('Validar retorno 200', () => {
-                const token = Cypress.env('access_token');
+    describe('Plano de Contas - Tipo Procedimento - Retorna os planos de contas para tipos de procedimentos', () => {
 
-                cy.request({
-                    method: 'GET',
-                    url: '/api/v1/plano-de-contas/tipo-procedimento',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                }).then((response) => {
-                    // Verifica se o status é 200
-                    expect(response.status).to.eq(200);
+        it('Validar retorno 200', () => {
+            const token = Cypress.env('access_token');
 
-                    // Valida que a resposta é um array
-                    expect(response.body).to.be.an('array');
-                    expect(response.body.length).to.be.greaterThan(0);
+            cy.request({
+                method: 'GET',
+                url: '/api/v1/plano-de-contas/tipo-procedimento',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then((response) => {
+                // Verifica se o status é 200
+                expect(response.status).to.eq(200);
 
-                    // Valida o primeiro item da lista
-                    const item = response.body[0];
+                // Valida que a resposta é um array
+                expect(response.body).to.be.an('array');
+                expect(response.body.length).to.be.greaterThan(0);
 
-                    expect(item).to.have.property('id', 1);
-                    expect(item).to.have.property('tipo', null);
-                    expect(item).to.have.property('categoria', null);
+                // Valida o primeiro item da lista
+                const item = response.body[0];
 
-                    expect(item).to.have.property('subPrimaria').that.deep.equals({
-                        codigo: '3101005',
-                        nome: 'PRESTAÇÃO DE SERVIÇOS'
-                    });
+                expect(item).to.have.property('id', 1);
+                expect(item).to.have.property('tipo', null);
+                expect(item).to.have.property('categoria', null);
 
-                    expect(item).to.have.property('subSecundaria', null);
-                    expect(item).to.have.property('classificacao').that.deep.equals({
-                        codigo: '3101005001001',
-                        nome: 'Prestação de Consultas Médicas'
-                    });
+                expect(item).to.have.property('subPrimaria').that.deep.equals({
+                    codigo: '3101005',
+                    nome: 'PRESTAÇÃO DE SERVIÇOS'
                 });
+
+                expect(item).to.have.property('subSecundaria', null);
+                expect(item).to.have.property('classificacao').that.deep.equals({
+                    codigo: '3101005001001',
+                    nome: 'Prestação de Consultas Médicas'
+                });
+            });
+        })
+
+        it('Validar retorno 404', () => {
+            const token = Cypress.env('access_token');
+
+            cy.request({
+                method: 'DELETE',
+                url: '/api/v1/plano-de-contas/tipo-procedimento',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                failOnStatusCode: false
+            }).then((response) => {
+                // Verifica se o status é 404
+                expect(response.status).to.eq(404);
             })
+        })
+    })
 
-            it('Validar retorno 404', () => {
-                const token = Cypress.env('access_token');
+    describe('Plano de Contas - Receitas - Retorna os planos de contas de tipo operação receita', () => {
 
-                cy.request({
-                    method: 'DELETE',
-                    url: '/api/v1/plano-de-contas/tipo-procedimento',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    },
-                    failOnStatusCode: false
-                }).then((response) => {
-                    // Verifica se o status é 404
-                    expect(response.status).to.eq(404);
-                })
+        it('Validar retorno 200', () => {
+            const token = Cypress.env('access_token');
+
+            cy.request({
+                method: 'GET',
+                url: '/api/v1/plano-de-contas/receitas',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                // Verifica se o status é 200
+                expect(response.status).to.eq(200);
+
+                // Valida que a resposta é um array
+                expect(response.body).to.be.an('array');
+                expect(response.body.length).to.be.greaterThan(0);
+
+                // Valida o primeiro item da lista
+                const item = response.body[0];
+
+                expect(item).to.have.property('id', 1);
+                expect(item).to.have.property('planoDeContas', 'PRESTAÇÃO DE SERVIÇOS - Pres. de Cons. Médi.');
+                expect(item).to.have.property('planoDeContasCompleto', 'PRESTAÇÃO DE SERVIÇOS - Prestação de Consultas Médicas');
+            });
+        })
+
+        it('Validar retorno 404', () => {
+            const token = Cypress.env('access_token');
+
+            cy.request({
+                method: 'DELETE',
+                url: '/api/v1/plano-de-contas/tipo-procedimento',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                failOnStatusCode: false
+            }).then((response) => {
+                // Verifica se o status é 404
+                expect(response.status).to.eq(404);
+            })
+        })
+    })
+
+    describe('Plano de Contas - Despesas - Retorna os planos de contas de tipo operação despesa', () => {
+        it('Validar retorno 200', () => {
+            const token = Cypress.env('access_token');
+
+            cy.request({
+                method: 'GET',
+                url: '/api/v1/plano-de-contas/despesas',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                // Verifica se o status é 200
+                expect(response.status).to.eq(200);
+
+                // Valida que a resposta é um array
+                expect(response.body).to.be.an('array');
+                expect(response.body.length).to.be.greaterThan(0);
+
+                // Valida o primeiro item da lista
+                const item = response.body[0];
+
+                expect(item).to.have.property('id', 10);
+                expect(item).to.have.property('categoriaDespesa', 'IMPO. INCI. SOBR. VEND. - MUNICIPAIS - ISSQN');
+                expect(item).to.have.property('categoriaDespesaCompleto', 'IMPOSTOS INCIDENTES SOBRE VENDAS - MUNICIPAIS - ISSQN');
             })
         })
 
-        describe('Plano de Contas - Receitas - Retorna os planos de contas de tipo operação receita', () => {
-            
-             it('Validar retorno 200', () => {
-                const token = Cypress.env('access_token');
+        it('Validar retorno 404', () => {
+            const token = Cypress.env('access_token');
 
-                cy.request({
-                    method: 'GET',
-                    url: '/api/v1/plano-de-contas/receitas',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-                    // Verifica se o status é 200
-                    expect(response.status).to.eq(200);
-
-                    // Valida que a resposta é um array
-                    expect(response.body).to.be.an('array');
-                    expect(response.body.length).to.be.greaterThan(0);
-
-                    // Valida o primeiro item da lista
-                    const item = response.body[0];
-
-                    expect(item).to.have.property('id', 1);
-                    expect(item).to.have.property('planoDeContas', 'PRESTAÇÃO DE SERVIÇOS - Pres. de Cons. Médi.');
-                    expect(item).to.have.property('planoDeContasCompleto', 'PRESTAÇÃO DE SERVIÇOS - Prestação de Consultas Médicas');
-                });
-            })
-
-            it('Validar retorno 404', () => {
-                const token = Cypress.env('access_token');
-
-                cy.request({
-                    method: 'DELETE',
-                    url: '/api/v1/plano-de-contas/tipo-procedimento',
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    },
-                    failOnStatusCode: false
-                }).then((response) => {
-                    // Verifica se o status é 404
-                    expect(response.status).to.eq(404);
-                })
-            })
-        })
-
-        describe('Plano de Contas - Despesas - Retorna os planos de contas de tipo operação despesa', () => {
-            it('Validar retorno 200', () => {
-                const token = Cypress.env('access_token');
-
-                cy.request({
-                    method: 'GET',
-                    url: '/api/v1/plano-de-contas/despesas',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-                    // Verifica se o status é 200
-                    expect(response.status).to.eq(200);
-
-                    // Valida que a resposta é um array
-                    expect(response.body).to.be.an('array');
-                    expect(response.body.length).to.be.greaterThan(0);
-
-                    // Valida o primeiro item da lista
-                    const item = response.body[0];
-
-                    expect(item).to.have.property('id', 10);
-                    expect(item).to.have.property('categoriaDespesa', 'IMPO. INCI. SOBR. VEND. - MUNICIPAIS - ISSQN');
-                    expect(item).to.have.property('categoriaDespesaCompleto', 'IMPOSTOS INCIDENTES SOBRE VENDAS - MUNICIPAIS - ISSQN');
-                })
-            })
-
-            it('Validar retorno 404', () => {
-                const token = Cypress.env('access_token');
-
-                cy.request({
-                    method: 'POST',
-                    url: '/api/v1/plano-de-contas/despesas',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                    },
-                    failOnStatusCode: false
-                }).then((response) => {
-                    // Verifica se o status é 404
-                    expect(response.status).to.eq(404);
-                })
+            cy.request({
+                method: 'POST',
+                url: '/api/v1/plano-de-contas/despesas',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                failOnStatusCode: false
+            }).then((response) => {
+                // Verifica se o status é 404
+                expect(response.status).to.eq(404);
             })
         })
     })
 })
+
 
 
