e670fb07e90599d864041e567aff1d4e0b16a981
Ivan <ivan.santos@amorsaude.com>
2025-06-17 14:28:03 -0300

    Add Modulo Perfis

--- cypress/e2e/AWS/parcerias.cy.js
+++ cypress/e2e/AWS/parcerias.cy.js
@@ -49,74 +49,73 @@ describe('Módulo - Parcerias', () => {
 
       });
     })
+  })
 
-    describe('Módulo - Parcerias - Parceiros - Retorna uma lista de parceiros', () => {
+  describe('Módulo - Parcerias - Parceiros - Retorna uma lista de parceiros', () => {
 
-      it('Validar retorno 200', () => {
+    it('Validar retorno 200', () => {
 
-        const token = Cypress.env('access_token')
-        cy.request({
-          method: 'GET',
-          url: '/api/v1/parcerias/parceiros', // URL do seu endpoint
-          headers: {
-            'Authorization': `Bearer ${token}`
-          },
-        }).then((response) => {
-          // Verificar se o status é 200
-          expect(response.status).to.eq(200);
+      const token = Cypress.env('access_token')
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/parcerias/parceiros', // URL do seu endpoint
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+      }).then((response) => {
+        // Verificar se o status é 200
+        expect(response.status).to.eq(200);
 
-          //Valida a estrutura dos dados
-          const data = response.body.data;
+        //Valida a estrutura dos dados
+        const data = response.body.data;
 
-          data.forEach((item, index) => {
-            expect(item).to.have.property('id');
-            expect(item).to.have.property('tipoId');
-            expect(item).to.have.property('nome');
-            expect(item).to.have.property('tabelaId');
-            expect(item).to.have.property('abrangenciaId');
-            expect(item).to.have.property('tipoCobrancaId');
-            expect(item).to.have.property('prazoFaturamento');
-            expect(item).to.have.property('dataBase');
-            expect(item).to.have.property('flagAtivo');
-            expect(item).to.have.property('createdAt');
-            expect(item).to.have.property('updatedAt');
-            expect(item).to.have.property('cnpj');
-            expect(item).to.have.property('razaoSocial');
-            expect(item).to.have.property('telefone');
-            expect(item).to.have.property('email');
-            expect(item).to.have.property('cep');
-            expect(item).to.have.property('endereco');
-            expect(item).to.have.property('numero');
-            expect(item).to.have.property('bairro');
-            expect(item).to.have.property('cidade');
-            expect(item).to.have.property('estado');
-            expect(item).to.have.property('nomeFantasia');
-            expect(item).to.have.property('telefoneSecundario');
-            expect(item).to.have.property('regiaoZona');
-            expect(item).to.have.property('emailAlternativo');
-            expect(item).to.have.property('complemento');
-            expect(item).to.have.property('urlParceiro');
-          })
+        data.forEach((item, index) => {
+          expect(item).to.have.property('id');
+          expect(item).to.have.property('tipoId');
+          expect(item).to.have.property('nome');
+          expect(item).to.have.property('tabelaId');
+          expect(item).to.have.property('abrangenciaId');
+          expect(item).to.have.property('tipoCobrancaId');
+          expect(item).to.have.property('prazoFaturamento');
+          expect(item).to.have.property('dataBase');
+          expect(item).to.have.property('flagAtivo');
+          expect(item).to.have.property('createdAt');
+          expect(item).to.have.property('updatedAt');
+          expect(item).to.have.property('cnpj');
+          expect(item).to.have.property('razaoSocial');
+          expect(item).to.have.property('telefone');
+          expect(item).to.have.property('email');
+          expect(item).to.have.property('cep');
+          expect(item).to.have.property('endereco');
+          expect(item).to.have.property('numero');
+          expect(item).to.have.property('bairro');
+          expect(item).to.have.property('cidade');
+          expect(item).to.have.property('estado');
+          expect(item).to.have.property('nomeFantasia');
+          expect(item).to.have.property('telefoneSecundario');
+          expect(item).to.have.property('regiaoZona');
+          expect(item).to.have.property('emailAlternativo');
+          expect(item).to.have.property('complemento');
+          expect(item).to.have.property('urlParceiro');
         })
       })
+    })
 
-      it('Validar retorno 404', () => {
+    it('Validar retorno 404', () => {
 
-        const token = Cypress.env('access_token')
-        cy.request({
-          method: 'POST',
-          url: '/api/v1/parcerias/parceiros', // URL do seu endpoint
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }, failOnStatusCode: false
-        }).then((response) => {
-          // Verificar se o status é 404
-          expect(response.status).to.eq(404);
+      const token = Cypress.env('access_token')
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/parcerias/parceiros', // URL do seu endpoint
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }, failOnStatusCode: false
+      }).then((response) => {
+        // Verificar se o status é 404
+        expect(response.status).to.eq(404);
 
-        });
-      })
+      });
     })
   })
+
 })
-
-
