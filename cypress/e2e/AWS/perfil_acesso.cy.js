3414d916a7749980f906d2e8a45be0537aa677fb
rrmqa12 <roger.mazzali@amorsaude.com>
2025-06-26 14:47:00 -0300

    Perfil de Acesso

---
+++ cypress/e2e/AWS/perfil_acesso.cy.js
@@ -0,0 +1,523 @@
+/// <reference types= "cypress" /> 
+
+describe('Módulo - Perfil de Acesso', () => {
+
+  beforeEach(() => {
+    cy.login()
+    cy.refreshToken()
+  });
+
+  describe('Módulo - Perfil de Acesso - Lista Perfis de acesso', () => {
+
+    it('Validar retorno 200 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso',
+        qs: {
+          page: 1,
+          limit: 10
+        },
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+
+        // Atribuir corretamente o body da resposta
+        const body = response.body;
+
+        // Validar que vieram 10 itens
+        expect(body.items).to.have.length(10);
+
+        // Valida que cada item do array tem as chaves esperadas
+        body.items.forEach((item) => {
+          expect(body.items[0]).to.have.all.keys('id', 'grupoDescricao', 'flg_status');
+        })
+
+        // Valida estrutura de meta
+        expect(body.meta).to.have.all.keys('totalItems', 'currentPage', 'itemCount', 'itemsPerPage', 'totalPages');
+      })
+    })
+
+    it('Validar retorno 400 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso', // Sem parâmetros na requisição da url
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+      })
+    })
+
+    it('Validar retorno 401 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          // 'Authorization': `Bearer ${token}`, // Token Inválido
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      })
+    })
+
+    it('Validar retorno 404 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'DELETE', // Método divergente
+        url: '/api/v1/perfil-acesso',
+        qs: {
+          page: 1,
+          limit: 10
+        },
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      })
+    })
+  })
+
+  describe('Módulo - Perfil de Acesso - Cria um novo Perfil de acesso', () => {
+
+    it('Validar retorno 201 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'testeQa',
+          recursos: [
+            7,
+            7,
+            7
+          ]
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(201)
+
+        const item = response.body;
+        expect(item).to.have.property('flagDeError', false);
+        expect(item).to.have.property('codigo', 201);
+        expect(item).to.have.property('mensagem', 'Perfil cadastrada com sucesso.');
+      })
+    })
+
+    it('Validar retorno 400 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json' // Sem parâmetro no body
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(400)
+      })
+    })
+
+    it('Validar retorno 401 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          //'Authorization': `Bearer ${token}`, // Token Inválido
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'testeQa',
+          recursos: [
+            7,
+            7,
+            7
+          ]
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(401)
+      })
+    })
+
+    it('Validar retorno 403 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',  // Método divergente
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'testeQa',
+          recursos: [
+            7,
+            7,
+            7
+          ]
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(403)
+      })
+    })
+
+    it('Validar retorno 404 - /api/v1/perfil-acesso', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'DELETE', // Método divergente
+        url: '/api/v1/perfil-acesso',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'testeQa',
+          recursos: [
+            7,
+            7,
+            7
+          ]
+        },
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(404)
+      })
+    })
+  })
+
+  describe('Módulo - Perfil de Acesso - Lista recursos', () => {
+
+    it('Validar retorno 200 - /api/v1/perfil-acesso/recursos', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso/recursos',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+
+        const items = response.body;
+        items.forEach((item) => {
+          expect(item).to.have.property('id');
+          expect(item).to.have.property('recDescLogico');
+          expect(item).to.have.property('recDescFisico');
+          expect(item).to.have.property('recTipoRecurso');
+          expect(item).to.have.property('subModule');
+        })
+
+      })
+    })
+
+    it('Validar retorno 400 - /api/v1/perfil-acesso/recursos', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'DELETE',
+        url: '/api/v1/perfil-acesso/recursos',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400)
+      })
+    })
+
+    it('Validar retorno 401 - /api/v1/perfil-acesso/recursos', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso/recursos',
+        headers: {
+          //'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401)
+      })
+    })
+
+    it('Validar retorno 404 - /api/v1/perfil-acesso/recursos', () => {
+      const token = Cypress.env('access_token');
+
+      cy.request({
+        method: 'POST',
+        url: '/api/v1/perfil-acesso/recursos',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404)
+      })
+    })
+  })
+
+  describe('Módulo - Perfil de Acesso - Atualiza o Perfil de acesso', () => {
+
+    it('Valdiar retorno 200 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'PUT',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'Teste AWS QA',
+          flg_status: 'true',
+          recursos: [
+            26,
+            63,
+            5
+          ]
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200)
+
+        expect(response.body).to.have.property('flagDeError', false);
+        expect(response.body).to.have.property('codigo', 201);
+        expect(response.body).to.have.property('mensagem', 'Perfil alterado com sucesso.');
+      })
+    })
+
+    it('Valdiar retorno 400 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'PUT',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: { // Sem parâmetro no body
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400)
+      })
+    })
+
+    it('Valdiar retorno 401 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'PUT',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          //'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'Teste AWS QA',
+          flg_status: 'true',
+          recursos: [
+            26,
+            63,
+            5
+          ]
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401)
+      })
+    })
+
+    it('Valdiar retorno 403 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'GET',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: {
+          descricaoAcessoProfile: 'Teste AWS QA',
+          flg_status: 'true',
+          recursos: [
+            26,
+            63,
+            5
+          ]
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(403)
+      })
+    })
+  })
+
+  describe('Módulo - Perfil de Acesso - Deleta o Perfil de acesso', () => {
+
+    it('Validar retorno 200 - /api/v1/perfil-acesso/{id}', () => {
+     const token = Cypress.env('access_token');
+     const idPerfil = 454;
+
+     cy.request({
+       method: 'DELETE',
+       url: `/api/v1/perfil-acesso/${idPerfil}`,
+       headers: {
+         'Authorization': `Bearer ${token}`,
+         'Content-Type': 'application/json'
+       },
+       failOnStatusCode: false,
+     }).then((response) => {
+       expect(response.status).to.eq(200)
+     })
+   })
+
+   it('Validar retorno 401 - /api/v1/perfil-acesso/{id}', () => {
+     const token = Cypress.env('access_token');
+     const idPerfil = 433;
+
+     cy.request({
+       method: 'DELETE',
+       url: `/api/v1/perfil-acesso/${idPerfil}`,
+       headers: {
+        // 'Authorization': `Bearer ${token}`, // Token Inválido
+         'Content-Type': 'application/json'
+       },
+       failOnStatusCode: false,
+     }).then((response) => {
+       expect(response.status).to.eq(401)
+     })
+   })
+
+   it('Validar retorno 404 - /api/v1/perfil-acesso/{id}', () => {
+     const token = Cypress.env('access_token');
+     const idPerfil = 433;
+
+     cy.request({
+       method: 'POST', // Método Divergente
+       url: `/api/v1/perfil-acesso/${idPerfil}`,
+       headers: {
+         'Authorization': `Bearer ${token}`,
+         'Content-Type': 'application/json'
+       },
+       failOnStatusCode: false,
+     }).then((response) => {
+       expect(response.status).to.eq(404)
+     })
+   })
+  })
+
+  describe('Módulo - Perfil de Acesso - Lista recursos pertencentes ao perfil de acesso', () => {
+
+    it('Validar retorno 200 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'GET',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      })
+    })
+
+    it('Validar retorno 400 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+    
+      cy.request({
+        method: 'GET',
+        url: '/api/v1/perfil-acesso/{id}', // Sem parâmetro ID
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+      })
+    })
+
+    it('Validar retorno 401 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'GET',
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          //'Authorization': `Bearer ${token}`, // Token Inválido
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(401);
+      })
+    })
+
+    it('Validar retorno 404 - /api/v1/perfil-acesso/{id}', () => {
+      const token = Cypress.env('access_token');
+      const idPerfil = 433;
+
+      cy.request({
+        method: 'POST', // Método divergente
+        url: `/api/v1/perfil-acesso/${idPerfil}`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      })
+    })
+  })
+})
