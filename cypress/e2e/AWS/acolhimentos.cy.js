6042185d716adaa310d8a440130e312c1b2c3cd4
rrmqa12 <roger.mazzali@amorsaude.com>
2025-09-15 16:16:33 -0300

    acolhimentos

---
+++ cypress/e2e/AWS/acolhimentos.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+/// >>>>>>>>>>>>>>>>>>>>>>>>>>    EM CONSTRUÇÃO  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<</
+
+describe('Módulo - Acolhimentos', () => {
+    beforeEach(() => {
+        cy.login();
+        cy.refreshtoken();
+    })
+
+    describe('Módulo - Acolhimentos - Cadastrar dados do acolhimento', () => {
+
+        it('Validar retorno 200 - /api/v1/acolhimentos', () => {
+
+        })
+    })
+})
